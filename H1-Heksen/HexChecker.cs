d1a46066f2f2dd798336c131158ebdd43afad278
GoosLife <72657308+GoosLife@users.noreply.github.com>
2022-08-20 12:43:39 +0200

    Add project files.

---
+++ H1-Heksen/HexChecker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace H1_Heksen
+{
+    public enum HexValidity
+    {
+        INCORRECT_LENGTH,
+        MISSING_HASH,
+        INVALID_INPUT,
+        VALID
+    }
+
+    /// <summary>
+    /// Tool to check if a string input is a valid hexadecimal color value (#000000-#FFFFFF).
+    /// </summary>
+    internal class HexChecker
+    {
+        public HexValidity Check(string input)
+        {
+            // If the input doesn't start with #
+            if (!HasHash(input[0]))
+            {
+                return HexValidity.MISSING_HASH;
+            }
+
+            // If the input is an incorrect length
+            if (!IsCorrectLength(input))
+            {
+                return HexValidity.INCORRECT_LENGTH;
+            }
+
+            // If the input contains characters other than 0-9, A-F
+            if (!HasOnlyValidCharacters(input.Substring(1)))
+            {
+                return HexValidity.INVALID_INPUT;
+            }
+
+            return HexValidity.VALID; // Input string passed all tests
+        }
+
+        private bool IsCorrectLength(string input)
+        {
+            return input.Length == 7; // All hexadecimal color values must be exactly 7 characters long.
+        }
+
+        private bool HasHash(char input)
+        {
+            return (input == '#');
+        }
+
+        private bool HasOnlyValidCharacters(string input)
+        {
+            Regex regHex = new Regex("[0-F]"); // Regular expression used to check if all characters fall within the 0-F range.
+
+            return regHex.IsMatch(input);
+        }
+    }
+}
---
+++ H1-Heksen/Program.cs
@@ -0,0 +1,42 @@
+﻿ using System;
+
+namespace H1_Heksen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HexChecker hc = new HexChecker();
+
+            HexValidity hv;
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Input your hex value: ");
+                string potentialHex = Console.ReadLine();
+
+                hv = hc.Check(potentialHex);
+
+                switch (hv)
+                {
+                    case HexValidity.MISSING_HASH:
+                        Console.WriteLine("\nPlease begin your hex value with the # symbol.\n");
+                        break;
+                    case HexValidity.INCORRECT_LENGTH:
+                        Console.WriteLine("\nPlease input exactly 7 characters, including the leading #\n");
+                        break;
+                    case HexValidity.INVALID_INPUT:
+                        Console.WriteLine("\nPlease only use the characters 0-9 and A-F\n");
+                        break;
+                    case HexValidity.VALID:
+                        Console.WriteLine("\nYour hex value was correct.\n");
+                        break;
+                }
+                Console.WriteLine("Press any key to continue . . . ");
+                Console.ReadKey();
+            } while (hv != HexValidity.VALID);
+        }
+    }
+}
